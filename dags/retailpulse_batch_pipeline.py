a3ca4d863eda0f6f933168cfe5c862d17a3263ed
vickymh84 <vicky.moharana84@gmail.com>
2026-09-15 05:13:58 +0530

    Update retailpulse_batch_pipeline.py

--- dags/retailpulse_batch_pipeline.py
+++ dags/retailpulse_batch_pipeline.py
@@ -1,1 +1,23 @@
+from datetime import datetime
 
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+
+with DAG(
+    dag_id="retailpulse_batch_pipeline",
+    start_date=datetime(2026, 9, 1),
+    schedule=None,
+    catchup=False,
+    tags=["retailpulse", "batch", "gcp"],
+) as dag:
+
+    start = EmptyOperator(
+        task_id="start"
+    )
+
+    end = EmptyOperator(
+        task_id="end"
+    )
+
+    start >> end
